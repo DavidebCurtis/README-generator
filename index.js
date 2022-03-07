36a268485ff6cbf07e239bbaba017911d16475b1
david curtis <heythereimdavidcurtis@gmail.com>
2022-03-06 23:32:08 -0500

    set up prompts

---
+++ index.js
@@ -0,0 +1,163 @@
+const inquirer = require("inquirer");
+const generatePage = require("./src/README-template.js");
+const fs = require("fs");
+
+const promptUser = (data) => {
+  if (!data) {
+    data = [];
+  }
+
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "githubname",
+        message: "What is your Github username?",
+        validate: (githubUsernameInput) => {
+          if (githubUsernameInput) {
+            return true;
+          } else {
+            console.log("Please enter your Github username.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your Email Address?",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter your Email Address.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "projectname",
+        message: "Please enter your project's name",
+        validate: (projectInput) => {
+          if (projectInput) {
+            return true;
+          } else {
+            console.log("Please enter your project username.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please enter a short description of your project",
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log("Please enter a description.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "checkbox",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "isc"],
+        validate: (licenseInput) => {
+          if (licenseInput) {
+            return true;
+          } else {
+            console.log("Please enter what license your project should have.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install dependencies?",
+        validate: (dependenciesInput) => {
+          if (dependenciesInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter what command should be run to install dependencies."
+            );
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?",
+        validate: (testsInput) => {
+          if (testsInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter what command should be run to run tests."
+            );
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "using",
+        message: "What does the user need to know about running the repo?",
+        validate: (usingInput) => {
+          if (usingInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter what the user need to know about running the repo."
+            );
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message:
+          "What does the user need to know about contributing to the repo?",
+        validate: (contributingInput) => {
+          if (contributingInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter what the user need to know about contributing to the repo."
+            );
+            return false;
+          }
+        },
+      },
+    ])
+    .then((answersData) => {
+      data.push(answersData);
+      return data;
+    });
+};
+
+// const create = fs.writeFile(
+//   "READMETEST.md",
+//   generatePage(description),
+//   (err) => {
+//     if (err) throw new Error(err);
+
+//     console.log("README.md created successfuly!");
+//   }
+// );
+
+promptUser()
+  .then((data) => {
+    console.log(data);
+  })
+  .then((data) => {
+    console.log(generatePage(data));
+    return generatePage(data);
+  });
---
+++ src/README-template.js
@@ -0,0 +1,9 @@
+const generatePage = (data) => {
+  //   return `
+  //     # Description
+  //     ${data[0].description}
+  //     `;
+  console.log(data);
+};
+
+module.exports = generatePage;
